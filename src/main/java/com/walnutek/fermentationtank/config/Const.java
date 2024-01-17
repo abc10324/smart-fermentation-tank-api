c52fc4323d84d8747c3c11adf4ca29a430631971
joyce <chihaya0811@gmail.com>
2024-01-17 17:12:05 +0800

    add collection const

--- src/main/java/com/walnutek/fermentationtank/config/Const.java
+++ src/main/java/com/walnutek/fermentationtank/config/Const.java
@@ -12,6 +12,13 @@ public class Const {
 
 	public static final String COLLECTION_USER = "user";
 	public static final String COLLECTION_LABORATORY = "laboratory";
+	public static final String COLLECTION_PROJECT = "project";
+	public static final String COLLECTION_FERMENTER = "fermenter";
+	public static final String COLLECTION_SENSOR = "sensor";
+	public static final String COLLECTION_SENSOR_RECORD = "sensorRecord";
+	public static final String COLLECTION_LINE_NOTIFY = "lineNotify";
+	public static final String COLLECTION_ALERT = "alert";
+	public static final String COLLECTION_ALERT_RECORD = "alertRecord";
 
 	public static final String BEARER_JWT = "Authorization";
 
