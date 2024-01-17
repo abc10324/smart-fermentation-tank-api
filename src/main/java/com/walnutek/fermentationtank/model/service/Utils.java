c6e41f9b6be33ff111f1edf07ff59053d26019b0
joyce <chihaya0811@gmail.com>
2024-01-17 17:20:30 +0800

    add hasArray

--- src/main/java/com/walnutek/fermentationtank/model/service/Utils.java
+++ src/main/java/com/walnutek/fermentationtank/model/service/Utils.java
@@ -5,10 +5,13 @@ import com.walnutek.fermentationtank.exception.AppException.Code;
 import org.springframework.data.mongodb.core.aggregation.Field;
 import org.springframework.data.mongodb.core.aggregation.Fields;
 import org.springframework.util.StringUtils;
+import org.yaml.snakeyaml.util.ArrayUtils;
 
 import java.io.Serializable;
 import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Array;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -22,10 +25,21 @@ public class Utils {
 		return StringUtils.hasText(Optional.ofNullable(value).map(String::valueOf).orElse(""));
 	}
 
+	public static Boolean hasArray(Object value) {
+		var result = false;
+		if (value.getClass().isArray()) {
+			var list = Arrays.asList((Object[])value);
+			if(!list.isEmpty()){
+				result = true;
+			}
+		}
+		return result;
+	}
+
 	public static String id() {
 		return "_id";
 	}
-	
+
 	public static <T> String field(SFunction<T,?> fieldGetter) {
 		var methodName = getMethodName(fieldGetter);
 
@@ -33,13 +47,13 @@ public class Utils {
 			methodName.substring(3);
 			char[] charArray = methodName.substring(3).toCharArray();
 			charArray[0] = Character.toLowerCase(charArray[0]);
-			
+
 			var result = new String(charArray);
-			
+
 			if("id".equals(result)) {
 				result = "_id";
 			}
-			
+
 			return result;
 		} else {
 			throw new AppException(Code.E000);
