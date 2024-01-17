9401fb1ffa89fd9070bc4d3854d5bcce64856f78
joyce <chihaya0811@gmail.com>
2024-01-17 17:23:01 +0800

    add getLoginUser

--- src/main/java/com/walnutek/fermentationtank/model/service/BaseService.java
+++ src/main/java/com/walnutek/fermentationtank/model/service/BaseService.java
@@ -1,12 +1,18 @@
 package com.walnutek.fermentationtank.model.service;
 
 import com.walnutek.fermentationtank.config.auth.Auth;
+import com.walnutek.fermentationtank.exception.AppException;
+import com.walnutek.fermentationtank.model.dao.UserDao;
+import com.walnutek.fermentationtank.model.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class BaseService {
+    @Autowired
+    protected UserDao userDao;
 
     protected String getLoginUserId() {
         String userId = null;
@@ -16,6 +22,11 @@ public class BaseService {
         }
 
         return userId;
+    }
+
+    protected User getLoginUser(){
+        return Optional.ofNullable(userDao.selectById(getLoginUserId()))
+                .orElseThrow(() -> new AppException(AppException.Code.E004));
     }
 
 }
